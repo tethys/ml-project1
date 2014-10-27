79b51a1f48c684aff4ccb079bd05a3ed2a4af5d8
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-10-27 19:00:18 +0100

    added comment in logisticRegression

--- logisticRegression.m
+++ logisticRegression.m
@@ -17,13 +17,23 @@ function beta = logisticRegression( varargin )
   end
   
   % Initialize algorithm parametes
-  maxIters = 900;
+  maxIters = 1000;
   beta = randn( size(tX, 2), 1 );
 
   for k = 1 : maxIters
       [cost, grad] = computeCostGradLogisticRegression(y, tX, beta, 0);
       beta = beta - alpha * grad;
-      fprintf(1, 'current cost %3.3f\n', cost)
+      fprintf(1, 'current cost %3.3f\n', cost);
+      
+     % viviana: for me this does not work..it gives 
+%      Warning: Matrix is singular, close to singular or
+% badly scaled. Results may be inaccurate. RCOND =
+% NaN. 
+     % sig = sigmoid( tX * beta );
+     % s = sig .* (1 - sig);
+     % g = computeGradientLogReg( y, tX, beta );
+     % inv_H = inv(tX' * diag(s) * tX);
+     % beta = beta - alpha .* inv_H * g;
   end
   
 end
