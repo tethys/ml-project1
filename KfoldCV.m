a1e45447d884501cc02e1eacc4bdc08fe7557f06
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-10-27 20:26:48 +0100

    fixes to KfoldCV for classification and a script that uses it for finding lambda

--- KfoldCV.m
+++ KfoldCV.m
@@ -1,16 +1,27 @@
 % K-fold cross validation for estimating test errors
-function tError = KfoldCV(K, X, y, mode)
+function [meanTrainError, meanValidationError]= KfoldCV(K, X, y, mode, varargin)
+% Chech the arguments
+
+    switch nargin
+    case 5
+        alpha = varargin{1};
+    case 6
+        alpha = varargin{1};
+        lambda = varargin{2};
+    otherwise
+        error('Unexpected number of input arguments');
+    end
 
     % split data in K fold (we will only create indices)
     setSeed(1);
-    K = 4;
     N = size(y,1);
     idx = randperm(N);
     Nk = floor(N/K);
     for k = 1:K
         idxCV(k,:) = idx(1+(k-1)*Nk:k*Nk);
     end
-
+    trainError = zeros(K,1); 
+    validationError = zeros(K, 1);
     for k = 1:K
         % get k'th subgroup in test, others in train
         idxTe = idxCV(k,:);
@@ -28,24 +39,21 @@ function tError = KfoldCV(K, X, y, mode)
 
         % calculate parameters
         if( mode == 0 )         %  leastSquaresGD
-            alpha = 0.1;
            [beta] = leastSquaresGD(yTr, tXTr, alpha);
         elseif( mode == 1 )     %  leastSquares
            [beta] = leastSquares(yTr, tXTr);
         elseif( mode == 2 )     %  ridgeRegression
-            lambda = 1.0;
            [beta] = ridgeRegression(yTr, tXTr, lambda);
         elseif( mode == 3 )     %  logisticRegression
-            alpha = 0.1;
            [beta] = logisticRegression(yTr, tXTr, alpha);
+           [trainError(k), ~] = computeCostGradLogisticRegression(yTr, tXTr, beta, 0);
+           [validationError(k), ~] = computeCostGradLogisticRegression(yTe, tXTe, beta, 0);
         else                    %  penLogisticRegression
-            alpha = 0.1;
-            lambda = 1.0;
            [beta] = penLogisticRegression(yTr, XTr, alpha, lambda);
-        end
-        
-        % testing MSE using least squares
-        tErrorSub(k) = computeCost(yTe, tXTe, beta);
+           [trainError(k), ~] = computeCostGradLogisticRegression(yTr, tXTr, beta, lambda);
+           [validationError(k), ~] = computeCostGradLogisticRegression(yTe, tXTe, beta, lambda);
+        end 
     end
-    tError = mean(tErrorSub);
+    meanTrainError = mean(trainError);
+    meanValidationError = mean(validationError);
 end
