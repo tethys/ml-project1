95f3f1e06fb58540b3fe158016cac3e7415fcb19
Angelopoulos Vasileios <billagelo@gmail.com>
2014-11-01 04:39:20 +0100

    Removing outliers for regression task + Question
    Here is the script to remove the outliers for the regression task. With
    some simple changes we can perform the same for the classification.
    Regarding the question that I have: since we have two models fitting in
    our regression task, how we will compute the test error that he asks in
    a .csv file?? If the error is the prediction of the error that the
    unknown test data will have, my opinion is find the mean error per
    sample for every model and after counting the samples that belong to
    each of the models for the test data, we just multiply with the
    respective portion factors  and we sum. What do you think??

--- load_regression_data.m
+++ load_regression_data.m
@@ -1,4 +1,4 @@
-function [X_train, y_train, X_test] = load_regression_data(th)
+function [X_train, y_train, X_test] = load_regression_data( th )
 
 clear data
 data = load('Rome_regression.mat');
@@ -7,31 +7,28 @@ D = size(data.X_train,2);
 if th == 0
     indices = (data.y_train < 4900);
 else
-    indices = (data.y_train >=4900);
+    indices = (data.y_train >= 4900);
 end
 y_train = data.y_train(indices);
 X_train = data.X_train(indices,:);
 
-%% Do we remove here outliers??
-
-N = size(X_train, 1);
-
-%% Feature number 14 is categorical, make it a dummy variable
+%% Make categorical features dummy variables
 categorical_data = X_train(:,D-6:end) + 1;
 temp = dummyvar(categorical_data);
-X_train = [X_train(:,1:(D-6)) temp];
+X_train = [X_train(:,1:(D-7)) temp];
 
 X_test = data.X_test;
 categorical_data = X_test(:,D-6:end) + 1;
 temp = dummyvar(categorical_data);
-X_test = [X_test(:,1:(D-6)) temp];
+X_test = [X_test(:,1:(D-7)) temp];
 
 %% Compute mean and std of the training set
 X_mean = mean(X_train);
 X_std = std(X_train);
 
-%% Normalize the data to have 0 mean and 1 std
-X_mean_rep = repmat(X_mean,[N, 1]);
+%% Normalize train data to have 0 mean and 1 std
+N = size(X_train, 1);
+X_mean_rep = repmat(X_mean,[N,1]);
 X_std_rep = repmat(X_std,[N,1]);
 X_train = X_train - X_mean_rep;
 X_train = X_train ./ X_std_rep;
@@ -42,5 +39,15 @@ X_mean_rep = repmat(X_mean, [N,1]);
 X_std_rep = repmat(X_std, [N,1]);
 X_test = X_test - X_mean_rep;
 X_test = X_test ./ X_std_rep;
+
+%% Remove outliers from the train data
+% If the data are normally distributed with mean 0 and std 1, then the
+% 99.9% of them are appear between the values -3.291 and 3.291
+[row_to_remove, ~] = find(X_train < -3.291);
+[temp, ~] = find(X_train > 3.291);
+row_to_remove = unique([row_to_remove ; temp]);
+row_to_remove = sort(row_to_remove,'descend');
+X_train(row_to_remove,:)=[];
+y_train(row_to_remove,:)=[];
 
 end
